0a24b9438b44310b790def67c48227a47dc8fc4f
Benjamin Fahl <Benjamin.Fahl@rwth-aachen.de>
2023-05-24 09:45:38 +0200

    small changes

--- 1r/old/Fit_example.py
+++ 1r/old/Fit_example.py
@@ -1,31 +1,26 @@
-from diffpy.srfit.fitbase import FitResults, initializeRecipe
+from diffpy.srfit.fitbase import FitResults
 from matplotlib import pyplot as plt
 from ezfit import FitPDF, Contribution
 from ezpdf import plot_PDF
 from pathlib import Path
 from glob import glob
 
-names = glob("../../0d/gr30/*.gr")
-for name in names:
-    path_to_file = name
-    name = Path(name).stem
-    print(name)
-    path_to_res = glob(f"../../0d/res/{name}*.res")[0]
-    Al2O3 = Contribution(cif_name="Al2O3", cf_name="sphericalCF", formula="Al2O3")
-    NiO = Contribution(cif_name="NiO", cf_name="sphericalCF", formula="NiO")
-    Ni = Contribution(cif_name="Ni", cf_name="sphericalCF", formula="Ni")
-    fit = FitPDF(path_to_file, contributions=[Ni, Al2O3, NiO])
-    fit.config["rmin"] = 1
-    fit.config["rmax"] = 5
-    fit.config["rstep"] = 0.1
-    fit.update_recipe()
-    fit.recipe.free("all")
-    initializeRecipe(fit.recipe, path_to_res)
-    fit.res = FitResults(fit.recipe)
-    fig, ax = plot_PDF(fit, fit.recipe, fit.res)
-    fit.dw.save_results(recipe=fit.recipe,
-                        footer=f"Fit of {name}",
-                        directory="./res/",
-                        file_stem=name,
-                        pg_names=fit.phases)
-    plt.savefig(f"./res/{name}.pdf")
+gr = glob("../../0d/gr30/*.gr")[0]
+Al2O3 = Contribution(cif_name="Al2O3", cf_name="sphericalCF", formula="Al2O3")
+NiO = Contribution(cif_name="NiO", cf_name="sphericalCF", formula="NiO")
+Ni = Contribution(cif_name="Ni", cf_name="sphericalCF", formula="Ni")
+
+fit = FitPDF(path_to_file, contributions=[Ni, Al2O3, NiO])
+
+fit.update_recipe()
+fit.run_fit()
+fit.res = FitResults(fit.recipe)
+fig, ax = plot_PDF(fit, fit.recipe, fit.res)
+
+name = Path(gr).stem
+fit.dw.save_results(recipe=fit.recipe,
+                    footer=f"Fit of {name}",
+                    directory="./res/",
+                    file_stem=name,
+                    pg_names=fit.phases)
+plt.savefig(f"./res/{name}.pdf")
--- 1r/old/diff/plot_diff.py
+++ 1r/old/diff/plot_diff.py
@@ -14,13 +14,13 @@ c = ['r', 'r', 'r', 'b', 'b', 'b', 'g', 'g', 'g', 'k', 'k', 'k']
 for i, (f, c) in enumerate(zip(np.array(files)[num], c)):
     r, d = np.loadtxt(f).T
     ax.plot(r, d + i * 0.5, c=c)
-ax.plot([-1, -2], [0, 0], c='r', label='First Methanation')
-ax.plot([-1, -2], [0, 0], c='b', label='First Dropout')
-ax.plot([-1, -2], [0, 0], c='g', label='last Methanation')
-ax.plot([-1, -2], [0, 0], c='k', label='last Dropout')
-ax.legend()
+ax.plot([-1, -2], [0, 0], c='r', label='1. M')
+ax.plot([-1, -2], [0, 0], c='b', label='1. D')
+ax.plot([-1, -2], [0, 0], c='g', label='5. M')
+ax.plot([-1, -2], [0, 0], c='k', label='5. D')
+ax.legend(ncol=4, loc='upper center', fontsize=10)
 ax.set_xlim(1.5, 30)
-ax.set_ylim(-0.5, 6.4)
+ax.set_ylim(-0.5, 7.5)
 # add titel\
 ax.set_title('$\Delta(r) = G(r)-G(r)_\mathrm{calc}$ always 1/3, 2/3 and 3/3 of Dropout/Methanation')
 plt.savefig('/home/ben/mt/img/diff.pdf')
